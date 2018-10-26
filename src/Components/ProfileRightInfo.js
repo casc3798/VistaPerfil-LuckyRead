4ec5dfae3123335a2a43f6286eb3e3a8c033a37e
Camilo Alejandro Sánchez Cruz <caasanchezcr@unal.edu.co>
2018-10-26 17:00:11 -0500

    Profile left y right completos, falta boton de subir foto de perfil

--- src/Components/ProfileRightInfo.js
+++ src/Components/ProfileRightInfo.js
@@ -7,13 +7,13 @@ class ProfileRightInfo extends Component {
       <div className="col-md-12">
         <div className="row">
           <div className="col-md-6">
-            <a href="http://youtube.com" className="btn btn-squared-default btn-primary" id="buttonFrag">
+            <a href="http://youtube.com" className="btn btn-squared-default btn-primary btn-block" id="buttonFrag">
               <i className="fa fa-book"></i>
               <h5>Fragmentos</h5>
             </a>
           </div>
           <div className="col-md-6">
-            <a href="http://youtube.com" className="btn btn-squared-default btn-success" id="buttonPref">
+            <a href="http://youtube.com" className="btn btn-squared-default btn-success btn-block" id="buttonPref">
               <i className="fa fa-mouse-pointer"></i>
               <h5>Preferencias</h5>
             </a>
@@ -21,13 +21,13 @@ class ProfileRightInfo extends Component {
         </div>
         <div className="row">
           <div className="col-md-6">
-            <a href="http://youtube.com" className="btn btn-squared-default btn-danger" id="buttonFav">
+            <a href="http://youtube.com" className="btn btn-squared-default btn-danger btn-block" id="buttonFav">
               <i className="fa fa-star-o"></i>
               <h5>Favoritos</h5>
             </a>
           </div>
           <div className="col-md-6">
-            <a href="http://youtube.com" className="btn btn-squared-default btn-info" id="buttonInfo">
+            <a href="http://youtube.com" className="btn btn-squared-default btn-info btn-block" id="buttonInfo">
               <i className="fa fa-vcard"></i>
               <h5>Informacion</h5>
             </a>
