5d0b8bf379576d969b6bc5bb81a18b64348c5849
Camilo Alejandro Sánchez Cruz <caasanchezcr@unal.edu.co>
2018-10-26 08:02:30 -0500

    Perfil izquierdo y derecho sin colores

--- src/Components/ProfileRightInfo.js
+++ src/Components/ProfileRightInfo.js
@@ -4,18 +4,35 @@ import '../Estilos/ProfileRightInfo.css';
 class ProfileRightInfo extends Component {
   render() {
     return (
-      <div>
-        <p>Fabulas sint noster ullamco ipsum, non dolore in velit se malis fabulas sed anim
-          quid, sed multos ubi esse, id fabulas sempiternum a culpa possumus se dolore
-          nisi an ne aut sint possumus, occaecat te anim cernantur. Qui eram veniam dolor
-          singulis, o minim magna te voluptate te velit proident relinqueret, tempor
-          transferrem possumus sint officia, fore ne fabulas, aute mandaremus illustriora
-          ut magna singulis illustriora, quem laborum graviterque. Ita veniam excepteur
-          qui te o aute velit dolore, ubi anim iis tempor te si quem legam multos
-          fabulas.In commodo ex appellat a id offendit transferrem ea occaecat malis
-          aliqua eu velit, summis ita deserunt in nam tamen enim esse probant, de sunt
-          distinguantur eu export se mandaremus hic eram, dolor deserunt laboris. E ex
-          duis aliquip si ingeniis anim eiusmod litteris. Multos do consequat hic amet.</p>
+      <div className="col-md-12">
+        <div className="row">
+          <div className="col-md-6">
+            <a href="http://youtube.com" className="btn btn-squared-default btn-primary" id="buttonFrag">
+              <i className="fa fa-book"></i>
+              <h5>Fragmentos</h5>
+            </a>
+          </div>
+          <div className="col-md-6">
+            <a href="http://youtube.com" className="btn btn-squared-default btn-success" id="buttonPref">
+              <i className="fa fa-mouse-pointer"></i>
+              <h5>Preferencias</h5>
+            </a>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-md-6">
+            <a href="http://youtube.com" className="btn btn-squared-default btn-danger" id="buttonFav">
+              <i className="fa fa-star-o"></i>
+              <h5>Favoritos</h5>
+            </a>
+          </div>
+          <div className="col-md-6">
+            <a href="http://youtube.com" className="btn btn-squared-default btn-info" id="buttonInfo">
+              <i className="fa fa-vcard"></i>
+              <h5>Informacion</h5>
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
